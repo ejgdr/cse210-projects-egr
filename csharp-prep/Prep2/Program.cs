24f84f6157a329de8dcbb8e6ab510948d959d158
ejgdr <gui20006@byui.edu>
2024-01-10 09:53:25 -0700

    Prep 2 Completed

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,83 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage?: ");
+        int grade = int.Parse(Console.ReadLine());
+        int reminder = reminder = grade % 10;
+        string letter;
+
+        // Calculation to the letter grade and correspondant sign
+        if (grade >= 90)
+        {
+            if (reminder < 3)
+            {
+                letter = "A-";
+            }    
+            else
+            {
+                letter = "A";
+            }    
+        }
+        else if (grade >= 80)
+        {
+            if (reminder < 3)
+            {
+                letter = "B-";
+            }
+            else if (reminder >= 7)
+            {
+                letter = "B+";
+            }
+            else
+            {
+                letter = "B";
+            }
+        }
+        else if (grade >= 70)
+        {
+            if (reminder < 3)
+            {
+                letter = "C-";
+            }
+            else if (reminder >= 7)
+            {
+                letter = "C+";
+            }
+            else
+            {
+                letter = "C";
+            }
+        }
+        else if (grade >= 60)
+        {
+            if (reminder < 3)
+            {
+                letter = "D-";
+            }
+            else if (reminder >= 7)
+            {
+                letter = "D+";
+            }
+            else
+            {
+                letter = "D";
+            }
+        }    
+        else
+        {
+            letter = "F";
+        }
+
+        // Display results    
+        Console.WriteLine($"Your letter grade is {letter}");
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed!");
+        }    
+        else
+        {
+            Console.WriteLine("Don't give up and you'll get it next time!");
+        }            
     }
 }
