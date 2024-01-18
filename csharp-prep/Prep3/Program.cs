b433d54dae7b53a4bce8e1547efe1d7ea2f55a2a
ejgdr <gui20006@byui.edu>
2024-01-18 14:32:32 -0700

    Prep 3 Completed

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int magicNumber = randomGenerator.Next(1, 100);
+
+        // Console.Write("What is the magic number? ");
+        // int magicNumber = int.Parse(Console.ReadLine());
+
+        int attempts = 0;
+
+        Console.Write("What is your guess? ");
+        int guess = int.Parse(Console.ReadLine());
+
+        while (guess != magicNumber)
+        {
+            if (guess > magicNumber) 
+            {
+                Console.WriteLine("Lower");
+                attempts += 1;
+            }
+            else
+            {
+                Console.WriteLine("Higher");
+                attempts += 1;
+            }
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+        }
+        
+        Console.WriteLine("You guessed it!"); 
+        Console.WriteLine($"You guessed in {attempts} attempts");        
     }
 }
