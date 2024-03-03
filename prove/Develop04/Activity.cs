7f1bf628d7a9fb0028c5420879c9252e905c8c5e
ejgdr <gui20006@byui.edu>
2024-03-02 18:09:20 -0700

    Develop04: Ask duration, runIt and endMessage methods working

--- prove/Develop04/Activity.cs
+++ prove/Develop04/Activity.cs
@@ -4,24 +4,49 @@ public class Activity
     private string _description;
     private int _duration;
     private string _ending;
+    private List<string> spinnerAnimation = new List<string>() {"|", "/", "-", "\\", "|", "/", "-", "\\"};
+
 
     public Activity(string actName, string descrip)
     {
         _activityName = actName;
         _description = descrip;
+        _duration = 1;
     }
     public void DisplayGreeting()
     {
-        Console.WriteLine($"{_activityName}.\n\n{_description}\n");
+        Console.WriteLine($"Welcome to the {_activityName}.\n\n{_description}\n");
     }
     public void AskDuration()
     {
         Console.Write("How long, in seconds, would you like for your session? ");
         _duration = int.Parse(Console.ReadLine());
     }
-    public void DisplayEndMessage(){}
-    public void RunIt(){}
-    public void ShowSpinner(){}
-    public void ShowCountdown(){}
+    public void DisplayEndMessage()
+    {
+        Console.WriteLine("Well done!!");
+        ShowSpinner();
+        Console.WriteLine($"You have completed another {_duration} seconds of the {_activityName}.");
+    }
+    public void RunIt()
+    {
+        Console.Clear();
+        Console.WriteLine("Get ready...");
+        ShowSpinner();
+    }
+    public void ShowSpinner()
+    {
+        foreach (string s in spinnerAnimation)
+        {
+            Console.Write(s);
+            Thread.Sleep(800);
+            Console.Write("\b \b");
+        }
+
+    }
+    public void ShowCountdown()
+    {
+
+    }
     
 }
--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -5,12 +5,11 @@ class Program
     {
         Menu();
     }
-
     static public void Menu()
-    {
-        
+    {     
         while (_selection != 4)
         {
+            Console.Clear();
             Console.WriteLine("Menu Options:");
             Console.WriteLine("  1. Start breathing activity\n  2. Start reflecting activity\n  3. Start listing activity\n  4. Quit");
             Console.Write("Select a choice from the menu: ");
@@ -19,23 +18,33 @@ class Program
             if (_selection == 1)
             {
                 BreathingActivity breathing = new BreathingActivity(
-                    "Welcome to the Breathing Activity",
+                    "Breathing Activity",
                     "This activity will help you relax by walking your through breathing in and out slowly. Clear your mind and focus on your breathing.");
                 breathing.DisplayGreeting();
                 breathing.AskDuration();
+                breathing.RunIt();
+                breathing.DisplayEndMessage();
 
             }
             else if (_selection == 2)
             {
-                ReflectingActivity reflecting = new ReflectingActivity("Welcome to the Reflection Activity", "This activity will help you reflect on times in your life when you have shown strength and resilience. This will help you recognize the power you have and how you can use it in other aspects of your life.");
+                ReflectingActivity reflecting = new ReflectingActivity(
+                    "Reflection Activity",
+                    "This activity will help you reflect on times in your life when you have shown strength and resilience. This will help you recognize the power you have and how you can use it in other aspects of your life.");
                 reflecting.DisplayGreeting();
                 reflecting.AskDuration();
+                reflecting.RunIt();
+                reflecting.DisplayEndMessage();
             }
             else if (_selection == 3)
             {
-                ListingActivity listing = new ListingActivity("Welcome to the Listing Activity", "This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.");
+                ListingActivity listing = new ListingActivity(
+                    "Listing Activity",
+                    "This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.");
                 listing.DisplayGreeting();
                 listing.AskDuration();
+                listing.RunIt();
+                listing.DisplayEndMessage();
             }
             else if (_selection == 4)
             {
