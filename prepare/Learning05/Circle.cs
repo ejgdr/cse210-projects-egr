ddef52f07377de0cc3e656e28726a683547e9a3a
ejgdr <gui20006@byui.edu>
2024-03-07 13:27:20 -0700

    Learning05: Completed

---
+++ prepare/Learning05/Circle.cs
@@ -0,0 +1,15 @@
+using System.Reflection.Metadata;
+
+public class Circle : Shape
+{
+    private double _radius;
+
+    public Circle(double radius, string color) : base(color)
+    {
+        _radius = radius;
+    }
+    public override double GetArea()
+    {
+        return Math.PI * Math.Pow(_radius, 2);
+    }
+}
--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square square1 = new Square(3, "Yellow");
+        shapes.Add(square1);
+
+        Rectangle rectangle1 = new Rectangle(3, 6, "Blue");
+        shapes.Add(rectangle1);
+
+        Circle circle1 = new Circle(3, "Red");
+        shapes.Add(circle1);
+
+        foreach (Shape shape in shapes)
+        {
+            double area = shape.GetArea();
+            string color = shape.GetColor();
+
+            Console.WriteLine($"The {color} shape has an area of {area}.");  
+        }
+        
+        
+        
+
+        
     }
 }
