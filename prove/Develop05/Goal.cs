1583eed99f3b15d2bb6565c135dc519f800b19af
ejgdr <gui20006@byui.edu>
2024-03-14 18:06:08 -0600

    Develop05: Rough draft of Goal

--- prove/Develop05/Goal.cs
+++ prove/Develop05/Goal.cs
@@ -1,8 +1,38 @@
-public class Goal
+public abstract class Goal
 {
     // Help to have all common attributes and behaviors of my Goals
-    public Goal()
+    protected string _type;
+    protected string _name;
+    protected string _description;
+    protected int _points;
+    protected bool _done;
+    public Goal(){}
+
+    public string Serialize()
     {
+        return $"{_type}~{_name}~{_description}~{_points}~{_done}";
     }
+    public void Deserialize(string content)
+    {
+        string[] splitting = content.Split("~");
+        if (splitting.Length < 4)
+        {
+            Console.WriteLine("Missing content");
+        }
+        else
+        {
+            _type = splitting[0];
+            _name = splitting[1];
+            _description = splitting[2];
+            _points = int.Parse(splitting[3]);
+            _done = bool.Parse(splitting[4]);
+        }
+    }
+
+    public abstract void DisplayGoal();
+    public abstract void ScoreCount();
+    public abstract void RecordEvent();
+    public abstract void MarkOffGoal();
+
 
 }
--- prove/Develop05/GoalManager.cs
+++ prove/Develop05/GoalManager.cs
@@ -1,8 +1,16 @@
 public class GoalManager
 {
     // Help to manage my score, display my goals, save them and load them
-    public GoalManager()
-    {
-    }
+    private FileManager _fileManage = new FileManager();
+    protected int _score;
+    protected Goal _goal;
+    protected List<Goal> _goals = new List<Goal>(){};
+    public GoalManager(){}
+
+    public void CurrentScore(){}
+    public void DisplayGoals(){}
+    public void SaveGoals(){}
+    public void LoadGoals(){}
+
 
 }
